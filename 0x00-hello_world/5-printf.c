7cccc2a314bd287d02a3485ed9b6e8be39592bad
Dunpaul <mwasmaish86@gmail.com>
2022-03-10 12:05:25 -0800

    printing

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints about grammer
+ *
+ * Return - 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
