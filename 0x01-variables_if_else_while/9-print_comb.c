f3b5a9a1bd6d1cee044bf6078f8ed9ff536e27a8
Dunpaul <mwasmaish86@gmail.com>
2022-03-11 11:29:42 -0800

    tf

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ *
+ * prints digits
+ *
+ * Return: o
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
