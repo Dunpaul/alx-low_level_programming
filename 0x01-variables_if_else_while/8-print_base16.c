ef1cfa7e61c9e783c0124fe0e051be0078316794
Dunpaul <mwasmaish86@gmail.com>
2022-03-11 11:10:56 -0800

    Hexadecimals

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ *
+ * prints hexadecimals
+ *
+ * Return: o
+ *
+ */
+
+int main(void)
+{
+	int d;
+	int c;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		putchar(d);
+	}
+	for (c = 'a'; c <='f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
