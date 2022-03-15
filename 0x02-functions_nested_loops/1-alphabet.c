b8c575ce2544b20c484260b66c6cc2e3ac1e95cc
Dunpaul <mwasmaish86@gmail.com>
2022-03-15 09:21:00 -0700

    alphabetz

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main -  entry point
+ * prints the letters of the alphabet
+ * retutn:0
+ */
+
+void print_alphabet(void)
+{
+	char ch = "a";
+
+	while (c <= "z")
+	{
+		_putchar(c);
+		ch++;
+	}
+	_putchar('\n');
+}
