989f28ccfa1eeb0aee5204169bcd1449c28f8e22
Dunpaul <mwasmaish86@gmail.com>
2022-03-30 07:17:37 -0700

    rev

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _print_rev_recursion - reverses a string
+ * @s: string to be reversed
+ *
+ * Return: nothing
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
