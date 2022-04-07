d3e74b43d6cec539905360d13cec1ac36d7d8570
Dunpaul <mwasmaish86@gmail.com>
2022-04-07 05:26:29 -0700

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main- prints the name of the function
+ * @argc: number of arguments input
+ * @argv: string values
+ *
+ * Return: nothing
+ */
+int main(int argc, char* argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
